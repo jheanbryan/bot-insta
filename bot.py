1531b86f3a0a27c5c27472111c8671820395e2f4
JHE4N <93744970+JHE4N@users.noreply.github.com>
2022-02-07 13:44:11 -0300

    Bot finalizado
    Segue um perfil

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 import time
 
-user = 'jhe4nbryan'
-senha = 'eumeamo123'
-seguir_este_user = 'hashtagprogramacao'
+print('--------BOT--------')
+user = input('Perfil: ')
+senha = input('Senha: ')
+seguir_este_user = input('Qual perfil deseja seguir? ')
 
 #criar navegador
 navegador = webdriver.Chrome('chromedriver.exe')
@@ -23,13 +24,9 @@ time.sleep(1)
 navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
 time.sleep(5)
 
-#salvar suas informações de login? Agora não
-#navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
-#time.sleep(3)
-
-# ativar notificações? Agora não
-navegador.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
-time.sleep(3)
+#clicar par ir na home
+#navegador.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[1]/div/a/svg').click()
+#time.sleep(1)
 
 #clicar na barra de pesquisa
 navegador.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[1]/div').click()
@@ -40,7 +37,7 @@ time.sleep(2)
 
 #clicar no nome do perfil a seguido (entrar nele)
 navegador.find_element_by_class_name('-qQT3').click()
-time.sleep(30)
+time.sleep(5)
 
 #clicar no botão de seguir
-navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button').click()
+navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
