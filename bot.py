7d1292c2d4003893857beea74d1475c8f9a09595
JHE4N <93744970+JHE4N@users.noreply.github.com>
2022-02-08 14:43:24 -0300

    Update bot.py
    Agora o bot segue os perfis que estiverem em: perfis_a_seguir.txt

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import time
 
-print('--------BOT--------')
+print('\n--------BOT--------')
 user = input('Perfil: ')
 senha = input('Senha: ')
-seguir_este_user = input('Qual perfil deseja seguir? ')
+print('Vou seguir os perfis do seguinte arquivo: perfis_a_seguir.txt\n')
 
 #criar navegador
 navegador = webdriver.Chrome('chromedriver.exe')
@@ -31,13 +31,21 @@ time.sleep(5)
 #clicar na barra de pesquisa
 navegador.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[1]/div').click()
 
-#digitar perfil a ser seguido
-navegador.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(seguir_este_user)
-time.sleep(2)
+with open('perfis_a_seguir.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+    for perfis in linhas:       
+        #digitar perfil a ser seguido
+        navegador.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(perfis)
+        time.sleep(2)
 
-#clicar no nome do perfil a seguido (entrar nele)
-navegador.find_element_by_class_name('-qQT3').click()
-time.sleep(5)
+        #clicar no nome do perfil a seguido (entrar nele)
+        navegador.find_element_by_class_name('-qQT3').click()
+        time.sleep(5)
 
-#clicar no botão de seguir
-navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
+        #clicar no botão de seguir
+        navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
+        time.sleep(15)
+        print('Perfil seguido: ' + perfis)
+
+print('Já segui todos da lista!')
+#navegador.close() #Esta linha fecha o navegador
